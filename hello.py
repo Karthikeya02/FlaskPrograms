03bb31f4961e51202fb93efcf5ac68c628e4a440
Karthikeya02 <86204780+Karthikeya02@users.noreply.github.com>
2023-05-13 14:21:46 +0530

    Created multiple pages

--- hello.py
+++ hello.py
@@ -2,9 +2,20 @@ from flask import Flask
 
 app = Flask(__name__)
 
+'''
+ROUTES
+TEMPLATES
+FORMS
+EXTENSIONS
+'''
 
 @app.route('/')
-def hello_world():
+def hompage():
+    return 'Welcome to the Home Page!'
+
+
+@app.route('/hello')
+def hello():
     return 'Hello, World!'
 
 
