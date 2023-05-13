e1c33f211947f57e570a0cd3761be64b82fed612
Karthikeya02 <86204780+Karthikeya02@users.noreply.github.com>
2023-05-13 14:32:20 +0530

    Small Change!

--- hello.py
+++ hello.py
@@ -21,7 +21,7 @@ def hello():
 # User Page
 @app.route('/user/<username>')
 def show_user_profile(username):
-    return 'Hello, %s' % username
+    return 'Hello, %s!' % username
 
 
 if __name__ == '__main__':
