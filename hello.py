344c77c59b29d7cc38c1e6a9a0f45e2d76ab6c69
Karthikeya02 <86204780+Karthikeya02@users.noreply.github.com>
2023-05-13 14:30:41 +0530

    Capture Dynamic values from URL

--- hello.py
+++ hello.py
@@ -8,15 +8,20 @@ TEMPLATES
 FORMS
 EXTENSIONS
 '''
-
+# Home Page
 @app.route('/')
 def hompage():
     return 'Welcome to the Home Page!'
 
-
+# Hello Page
 @app.route('/hello')
 def hello():
     return 'Hello, World!'
+
+# User Page
+@app.route('/user/<username>')
+def show_user_profile(username):
+    return 'Hello, %s' % username
 
 
 if __name__ == '__main__':
